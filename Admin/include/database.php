2d938adf35a28c0c2b70e2993cb2218fe4163806
OTMANE KHROUF <othmanekhrouf@gmail.com>
2025-06-18 11:56:34 +0100

    client

---
+++ Admin/include/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$port = "3306";
+$dbname = "ecommerce";
+$users = "root";
+$password = "";
+           $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $users, $password);
+
+
+
+?>
---
+++ ecran.php
@@ -0,0 +1,172 @@
+<?php
+require_once 'Admin/include/database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Ecran-Enritech</title>
+        <link rel="shortcut Icon" type="image/x-icon" href="./img/logo.png">
+
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <header>
+        <!-- TOP HEADER -->
+        <div id="top-header">
+            <div class="container">
+                <ul class="header-links pull-left">
+                    <li><a href="#"><i class="fa fa-phone"></i> +212661488101 | +212661486595 </a></li>
+                    <li><a href="mailto:contact@enritech.net"><i class="fa fa-envelope-o"></i> contact@enritech.net</a></li>
+                    <li><a href="#"><i class="fa fa-map-marker"></i>Entresol N° 76, Residence Chahbae D, Av. Louis Van Beethoven , Tangier 90000</a></li>
+                </ul>
+                <ul class="header-links pull-right">
+                    <li><a href="https://www.facebook.com/enritechnology/"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://x.com/enritechnology"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://www.instagram.com/enriquetechnology/"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/enrique-technology-solution-19b68b339/"><i class="fa fa-linkedin"></i></a></li>	
+                </ul>
+            </div>
+        </div>
+        <!-- /TOP HEADER -->
+
+        <!-- MAIN HEADER -->
+        <div id="header">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <!-- LOGO -->
+                    <div class="col-md-3">
+                        <div class="header-logo">
+                            <a href="https://enritech.net/" class="logo">
+                                <img src="./img/3.png" alt="ENRIQUE TECHNOLOGY" >
+                            </a>
+                        </div>
+                    </div>
+                    <!-- /LOGO -->
+
+                    <!-- SEARCH BAR -->
+                    <div class="col-md-6">
+                        <div class="header-search">
+                            <form>
+                                <input class="input" placeholder="Search here" required >
+                                <button class="search-btn">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /SEARCH BAR -->
+                    <!-- ACCOUNT -->
+                    <div class="col-md-3 clearfix">
+                        <div class="header-ctn">
+                            <!-- Cart -->
+                            <div class="dropdown">
+                                <div class="cart-dropdown">
+                                    <div class="cart-list">
+                                        <div class="product-widget">
+                                            <div class="product-img">
+                                                <img src="./img/product01.png" alt="">
+                                            </div>
+                                            <div class="product-body">
+                                                <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                                <h4 class="product-price"><span class="qty">1x</span>MAD 9800.00</h4>
+                                            </div>
+                                            <button class="delete"><i class="fa fa-close"></i></button>
+                                        </div>
+
+                                        <div class="product-widget">
+                                            <div class="product-img">
+                                                <img src="./img/product02.png" alt="">
+                                            </div>
+                                            <div class="product-body">
+                                                <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                                <h4 class="product-price"><span class="qty">3x</span>MAD 9800.00</h4>
+                                            </div>
+                                            <button class="delete"><i class="fa fa-close"></i></button>
+                                        </div>
+                                    </div>
+                                    <div class="cart-summary">
+                                        <small>3 Item(s) selected</small>
+                                        <h5>SUBTOTAL: MAD 2940.00</h5>
+                                    </div>
+                                    <div class="cart-btns">
+                                        <a href="#">View Cart</a>
+                                        <a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Cart -->
+
+                            <!-- Menu Toogle -->
+                            <div class="menu-toggle">
+                                <a href="#">
+                                    <i class="fa fa-bars"></i>
+                                    <span>Menu</span>
+                                </a>
+                            </div>
+                            <!-- /Menu Toogle -->
+                        </div>
+                    </div>
+                    <!-- /ACCOUNT -->
+                </div>
+                <!-- row -->
+            </div>
+            <!-- container -->
+        </div>
+        <!-- /MAIN HEADER -->
+    </header>
+
+    <!-- NAVIGATION -->
+     
+		<nav id="navigation">
+			<!-- container -->
+			<div class="container">
+				<!-- responsive-nav -->
+				<div id="responsive-nav">
+					<!-- NAV -->
+					<ul class="main-nav nav navbar-nav">
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="Ordinateur.php">Ordinateur</a></li>
+						<li class="active"><a href="ecran.php">Ecran</a></li>
+						<li><a href="Imprimante_Scanner.php">Imprimante & Scanner</a></li>
+						<li><a href="Accessoire _Périphérique.php">Accessoire & Périphérique</a></li>
+						<li><a href="Logiciel_OS.php">Logiciel & OS</a></li>
+						<li><a href="Réseau.php">Réseau </a></li>
+						<li><a href="Serveur.php">Serveur</a></li>
+						<li><a href="Tablette_Téléphone.php">Tablette & Téléphone</a></li>
+						<li><a href="Image_Son.php">Image & Son</a></li>
+						<li><a href="Consommables.php">Consommables</a></li>
+						<li><a href="Onduleur.php">Onduleur</a></li>
+					</ul>
+					<!-- /NAV -->
+				</div>
+				<!-- /responsive-nav -->
+			</div>
+			<!-- /container -->
+		</nav>
+
+	<!-- /NAVIGATION -->
+
+       
+
+</body>
+</html>
